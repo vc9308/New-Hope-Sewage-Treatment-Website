a6dab1c5478673e14fa5dcf613a40160de568900
vc9308 <vc93@icloud.com>
2017-02-24 17:34:32 +0800

    important commit

---
+++ js/myjs.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    height_r1 = $(".row1").height();
+    document.getElementById("c-b1").style.height = height_r1 + 20 + "px";
+    document.getElementById("c-b2").style.height = height_r1 + 45 + "px";
+});
